1a2c47d4c4aa224931b1530ea4d2305b0615501e
adahmo <adamumj@gmail.com>
2025-05-25 19:39:48 -0700

    updated backend3

--- backend.tf
+++ backend.tf
@@ -9,12 +9,14 @@ terraform {
 }
 */ 
 
- required_version = ">=0.13.0"
+ 
+terraform {
+required_version = ">=0.13.0"
   required_providers {
     aws = {
       version = ">= 2.7.0"
       source = "hashicorp/aws"
     }
   }
-
+}
 
